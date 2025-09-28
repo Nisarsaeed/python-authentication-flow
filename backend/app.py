c1225f94441a6ef88061fb76a6307c2f1bcc2a0b
Nisarsaeed <dash75549@gmail.com>
2025-09-28 08:16:50 +0500

    update: comments added to config

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,7 @@ from extensions import db, bcrypt, jwt, mail
 from routes.auth import auth_ns 
 from routes.admin import admin_ns
 from routes.user import user_ns
-from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from flask import send_from_directory
 from datetime import timedelta
 
 def create_app():
@@ -17,16 +15,17 @@ def create_app():
     # Config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # change later
+    #upload folder to store the profile images
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), "uploads/profile_images")
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-      # Flask-Mail config
+    # Flask-Mail config
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")   # set in .env
     app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")   # Gmail App Password
-    app.config["JWT_SECRET_KEY"] = "super-secret-key"  # change in production
+    # JWT config
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")  
     app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
     app.config["JWT_COOKIE_SECURE"] = False  # True in production with HTTPS
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False  # keep False until you setup CSRF
